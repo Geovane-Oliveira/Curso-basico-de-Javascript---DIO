efaa2849725a3435e46282a98e0c0995c7f4349c
Geovane-Oliveira <speedtestf1@gmail.com>
2021-05-23 22:03:32 -0300

    Aula 1 - Introducao ao Javascript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Tipagem dinâmica
+var nome = "Geovane Oliveira";
+var idade = 23;
+var idade2 = 24;
+var frase = "Cavaco eh careca"
+//alert(nome + " tem" + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+//console.log(frase.replace("Cavco","Geovane"))
+console.log(frase.toUpperCase());
+//alert(frase.replace("Cavaco", "Geovane"));
